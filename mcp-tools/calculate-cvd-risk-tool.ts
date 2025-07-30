6b04f9b10d48d74987c32c18d99d430e29e08b6f
James Dinh <jamesdinh77@gmail.com>
2025-07-29 22:24:43 -0700

    updated ports

--- mcp-tools/calculate-cvd-risk-tool.ts
+++ mcp-tools/calculate-cvd-risk-tool.ts
@@ -1,27 +1,46 @@
 import { z } from "zod";
-import { getFhirContext, getFhirResource, getPatientIdIfContextExists } from "../fhir-utilities";
+import {
+  getFhirContext,
+  getFhirResource,
+  getPatientIdIfContextExists,
+} from "../fhir-utilities";
 import { createTextResponse } from "../mcp-utilities";
 import axios from "axios";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp";
 import { Request } from "express";
 import { IMcpTool } from "../IMcpTool";
 import { Patient } from "../utils/patient";
-import { getPatientSex, getPatientAge, getPatientRace, getPatientName } from "../utils/patient-demographics";
-import { getPatientCholesterol, getPatientHDL, getPatientSystolicBloodPressure } from "../utils/patient-vitals";
-import { getPatientDiabetesStatus, getPatientSmokingStatus, getPatientHypertensionStatus } from "../utils/patient-conditions";
+import {
+  getPatientSex,
+  getPatientAge,
+  getPatientRace,
+  getPatientName,
+} from "../utils/patient-demographics";
+import {
+  getPatientCholesterol,
+  getPatientHDL,
+  getPatientSystolicBloodPressure,
+} from "../utils/patient-vitals";
+import {
+  getPatientDiabetesStatus,
+  getPatientSmokingStatus,
+  getPatientHypertensionStatus,
+} from "../utils/patient-conditions";
 import calculateCVDRisk from "../utils/calculate-cvd-risk";
 
 class CalculateCvdRiskTool implements IMcpTool {
   registerTool(server: McpServer, req: Request) {
     server.tool(
       "calculate_cvd_risk",
-      "Calculates the cardiovascular disease (CVD) 10-year risk based on patient attributes (Sex, Age, Race, Total Cholesterol, HDL - Cholesterol, Systolic Blood Pressure, Diabetes status, Current Smoking status, Treatment for Hypertension status).",
+      "Calculates the cardiovascular disease (CVD) 10-year risk",
       {
         patientID: z.string(),
       },
       async ({ patientID }) => {
+        console.log("mcp tool");
         const fhirContext = getFhirContext(req);
         if (!fhirContext) {
+          console.log("no fhir context");
           return createTextResponse(
             "A FHIR server url or token was not provided in the HTTP context.",
             { isError: true }
@@ -31,66 +50,83 @@ class CalculateCvdRiskTool implements IMcpTool {
         const patientIdContext = getPatientIdIfContextExists(req);
         const effectivePatientId = patientIdContext || patientID; // patientIdContext if exists, otherwise use patientID
         if (!effectivePatientId) {
+          console.log("no patient ID");
           return createTextResponse(
             "No patient ID provided or found in context.",
             { isError: true }
           );
         }
+        console.log("patient ID:", patientIdContext);
 
         const headers = {
           Authorization: `Bearer ${fhirContext.token}`,
-        }
+        };
 
         // Get Patient resource (patient structure can be found on https://www.hl7.org/fhir/R4/patient.html)
         // FHIR Patient resource for retrieving patient demographics
         const { data: patientResource } = await axios.get(
-            `${fhirContext.url}/Patient/${patientID}`,
-            { headers }
-          );
-        
+          `${fhirContext.url}/Patient/${effectivePatientId}`,
+          { headers }
+        );
+        console.log("patient resource:", patientResource);
+
         // FHIR Observations for retrieving patient vitals and conditions
         const observations = await getFhirResource(
           fhirContext,
           "Observation",
-          patientID
+          effectivePatientId
         )
-        .then((res) => {
-          if (!res.entry?.length) {
-            return null
-          }
+          .then((res) => {
+            if (!res.entry?.length) {
+              return null;
+            }
 
-          return res.entry
-            .map((x) => x.resource.code?.coding?.map((y) => y.display) || [])
-            .reduce((a, b) => a.concat(b), []);
-        })
+            return res.entry
+              .map((x) => x.resource.code?.coding?.map((y) => y.display) || [])
+              .reduce((a, b) => a.concat(b), []);
+          })
+          .catch((error) => {
+            console.error("Error fetching observations:", error);
+            return null;
+          });
+        console.log("observations:", observations);
 
-        // Helper functions to parse fields
-        const name = getPatientName(patientResource);
-        const age = getPatientAge(patientResource);
-        const gender = getPatientSex(patientResource);
-        const race = getPatientRace(patientResource).join(", ");
-        const totalCholesterol = getPatientCholesterol(observations);
-        const hdl = getPatientHDL(observations);
-        const systolicBloodPressure = getPatientSystolicBloodPressure(observations);
-        const conditions = {
-          smoker: getPatientSmokingStatus(observations),
-          diabetic: getPatientDiabetesStatus(observations),
-          hypertensive: getPatientHypertensionStatus(observations),
-        };
-        
-        const patient: Patient = {
-          name,
-          age,
-          gender,
-          race,
-          totalCholesterol,
-          hdl,
-          systolicBloodPressure,
-          conditions,
-        };
+        try {
+          // Helper functions to parse fields
+          const name = getPatientName(patientResource);
+          const age = getPatientAge(patientResource);
+          const gender = getPatientSex(patientResource);
+          const race = getPatientRace(patientResource).join(", ");
+          const totalCholesterol = getPatientCholesterol(observations);
+          const hdl = getPatientHDL(observations);
+          const systolicBloodPressure =
+            getPatientSystolicBloodPressure(observations);
+          const conditions = {
+            smoker: getPatientSmokingStatus(observations),
+            diabetic: getPatientDiabetesStatus(observations),
+            hypertensive: getPatientHypertensionStatus(observations),
+          };
 
-        const result = calculateCVDRisk(patient);
-        return createTextResponse(`Predicted 10-year Cardiovascular Disease risk score: ${result} for patient ${patient.toString()}.`,);
+          const patient: Patient = {
+            name,
+            age,
+            gender,
+            race,
+            totalCholesterol,
+            hdl,
+            systolicBloodPressure,
+            conditions,
+          };
+
+          const result = calculateCVDRisk(patient);
+          console.log("calculated risk:", result);
+          return createTextResponse(
+            `Predicted 10-year Cardiovascular Disease risk score: ${result} for patient ${patient.toString()}.`
+          );
+        } catch (error) {
+          console.error("Error", error);
+          return createTextResponse("error occured");
+        }
       }
     );
   }
