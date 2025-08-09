630e348520b7cba5e5db10b784ef826b86c51adc
James Dinh <jamesdinh77@gmail.com>
2025-08-08 18:04:06 -0700

    test debug revert

--- mcp-tools/index.ts
+++ mcp-tools/index.ts
@@ -1,13 +1,13 @@
 import { FindPatientIdToolInstance } from "./find-patient-id-tool";
 import { ReadFhirResourceToolInstance } from "./read-fhir-resource-tool";
 import { GetFhirResourceToolInstance } from "./get-patient-fhir-resource";
-// import { GetPatientAttributeToolInstance } from "./get-patient-attribute-tool";
+import { GetPatientAttributeToolInstance } from "./get-patient-attribute-tool";
 import { CalculateCvdRiskToolInstance } from "./calculate-cvd-risk-tool";
 
 export {
   FindPatientIdToolInstance,
   ReadFhirResourceToolInstance,
   GetFhirResourceToolInstance,
-  // GetPatientAttributeToolInstance,
+  GetPatientAttributeToolInstance,
   CalculateCvdRiskToolInstance,
 };
